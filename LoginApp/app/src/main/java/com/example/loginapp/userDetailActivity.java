c495d02a9f337a9fc305f5412764626cc7304d0e
Emily Tran <trane@oxy.edu>
2018-02-21 10:01:16 -0800

    Added Google API

--- LoginApp/app/src/main/java/com/example/loginapp/userDetailActivity.java
+++ LoginApp/app/src/main/java/com/example/loginapp/userDetailActivity.java
@@ -2,10 +2,12 @@ package com.example.loginapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -21,6 +23,8 @@ public class userDetailActivity extends AppCompatActivity {
     private TextView welcomeText;
     private boolean yesChecked;
     private boolean noChecked;
+    private EditText latitudeEditText;
+    private EditText longitudeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,8 @@ public class userDetailActivity extends AppCompatActivity {
         mContext = this;
         goBackButton = findViewById(R.id.go_back_button);
         welcomeText = findViewById(R.id.welcome_view);
+        latitudeEditText = findViewById(R.id.latitude);
+        longitudeEditText = findViewById(R.id.longitude);
 
 
         goBackButton.setOnClickListener(new View.OnClickListener(){
@@ -62,4 +68,35 @@ public class userDetailActivity extends AppCompatActivity {
     public void noChecked(View view){
         noChecked = ((CheckBox) view).isChecked();
     }
+
+    public void openMaps(View view){
+
+        String latitude = latitudeEditText.getText().toString();
+        String longitude = longitudeEditText.getText().toString();
+
+        // APIs for location stuff
+        // send a static string with location - longitude and latitude
+        Uri geoLocation = null;  // Uri is used to created URLs
+        //String location = "geo:34.1260, 118.2142";
+        //String location = "google.streetview:cbll=34.1260,118.2142)";
+        String location = "geo:" + latitude + "," + longitude;
+        geoLocation = Uri.parse(location);
+
+        // set up an intent
+        // pack geoloacation to the intent
+        // start intent
+
+        Intent intent = new Intent(Intent.ACTION_VIEW); // implicit, no destination needed
+        intent.setData(geoLocation);
+
+        // if there is any app that can receive this intent,
+        // then, start the app with the intent
+
+        System.out.println(intent.resolveActivity(getPackageManager()));
+        if(intent.resolveActivity(getPackageManager()) != null){    // is there any app that can receive and open this?
+            startActivity(intent);
+        }
+
+    }
+
 }
