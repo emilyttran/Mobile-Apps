4cd4ffc010ed5f76ea09360f5a62d18cd5886b2a
Emily Tran <trane@oxy.edu>
2018-02-21 10:12:57 -0800

    Added notifications

--- LoginApp/app/src/main/java/com/example/loginapp/MainActivity.java
+++ LoginApp/app/src/main/java/com/example/loginapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.loginapp;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -42,12 +45,39 @@ public class MainActivity extends AppCompatActivity {
     private void launchActivity(){
         // package intent and start activity
 
+        // build notification for this launch
+        // March 23, you onl need to pass the context as parameter
+        // now, you need to pass context and channel id -> "default"
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
+
+        // set icons
+        builder.setSmallIcon(android.R.drawable.star_big_off);
+
+        // if you want to use anything from this context, you can add it to the intent
+        // then get the data from intent
+        // msg: "username, you have logged in"
+
         //intent with from and to
         Intent intent = new Intent(mContext, userDetailActivity.class);
 
         // add data to the intent
         intent.putExtra("username",username.getText().toString());
         // start activity with the intent
+
+        // create a pending intent for the notification with the intent I created
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+
+        builder.setContentIntent(pendingIntent);
+
+        // set the title and content of the notification
+        builder.setContentTitle("Successfully logged in");
+        builder.setContentText("Hi " + intent.getExtras().getString("username"));
+
+        // get the system service to display this notification
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        
+        // notify and build the notification
+        notificationManager.notify(1, builder.build());
 
         // because you want to get results from an activity instead of just display, you can't use startActivity(intent);
         // instead,
